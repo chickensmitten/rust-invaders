250544826e91aa22aa4296cc1a5b06061d94d150
sheng yeong <chia.shengyeong@gmail.com>
2024-03-18 11:24:55 +0700

    added terminal load

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error, 
+    io::{self, stdout}
+};
+use crossterm::{
+    cursor::{Hide, Show},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand
+};
+use rusty_audio::Audio;
+
+fn main() -> Result <(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "explode.wav");
+    audio.add("lose", "lose.wav");
+    audio.add("move", "move.wav");
+    audio.add("pew", "pew.wav");
+    audio.add("startup", "startup.wav");
+    audio.add("win", "win.wav");
+    audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?; // it is like using nano or console to show alternative screens
+    stdout.execute(Hide)?; // Hide cursor
+
+    // Cleanup
+    audio.wait();
+    // cleanly close the screens
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
